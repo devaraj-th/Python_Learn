baf04f2e02e270aa818fd79799f02c06d8789b55
DEVARAJU TH <devarajuth106@gmail.com>
2025-10-16 21:36:37 +0000

    hello world program

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+name = str(input("Enter your name :"))
+
+age = int(input("Enter your age :"))
+
+print("User name is {} and age is {}".format(name,age))
+
+print(f"In the year 2035 the age {name} will be age {age + 10}")
---
+++ string_concatenation.py
@@ -0,0 +1,8 @@
+""" + is used to add two strings"""
+
+print('a + b ')
+print("alice + bob ")
+
+print('alive'+ '45')
+
+print("a" * 5)
